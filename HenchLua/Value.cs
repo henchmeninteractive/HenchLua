69beecedff616b5dbda017793f7b15787c1e6389
Phill Djonov <phill@vec3.net>
2013-09-07 03:14:14 -0700

    Got rid of the silly Box.cs file. Most Value-related types now live in Value.cs.

--- HenchLua/Value.cs
+++ HenchLua/Value.cs
@@ -900,6 +900,44 @@ namespace Henchmen.Lua
 		}
 	}
 
+	internal sealed class NumBox
+	{
+		public double Value;
+
+		public NumBox()
+		{
+		}
+
+		public NumBox( double value )
+		{
+			this.Value = value;
+		}
+
+		public override string ToString()
+		{
+			return Value.ToString();
+		}
+	}
+
+	internal sealed class ValueBox
+	{
+		public Value Value;
+
+		public ValueBox()
+		{
+		}
+
+		public ValueBox( Value value )
+		{
+			this.Value = value;
+		}
+
+		public override string ToString()
+		{
+			return Value.ToString();
+		}
+	}
+
 	public interface IHasMetatable
 	{
 		Table Metatable { get; }
